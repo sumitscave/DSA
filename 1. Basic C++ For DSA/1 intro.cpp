a67ae64b1ddb33d44be326fc1f41f3aadcdb7938
Sumit Mukharjee <sumitxxmukharjee@hotmail.com>
2024-11-25 23:45:32 +0530

    Revised till input output

---
+++ 1. Basic C++ For DSA/1 intro.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    cout << " Hey Sumit" << "\n";
+    cout << "How are you";
+    // std::cout << "How are you";
+    //when you use using namespace std you dont have to use std
+    return 0;
+}
