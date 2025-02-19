2e8a892d7e46eca7bf31ff0b7610ed4899f5aec1
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-02-19 20:38:51 +0530

    Var and Constants

--- C++ Concepts/helloworld.cpp
+++ C++ Concepts/helloworld.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+// using std::cout;
+// using std::endl;
 int main(){
-    cout << "Hello World";
+    std::cout << "Hello World"<<std::endl;
     return 0;
 }
