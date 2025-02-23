3d7eabe7ab7f9b3d26f719bfc827f5c55439c119
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-02-23 23:59:08 +0530

    another modifiers

---
+++ C++ Concepts/casting.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int teaLeaves = 50;
+    float waterTemperature = 85.5;
+    double priceOfTea = 299.9999999999999999999999999;
+    char teaGrading = 'B';
+    bool isTeaReady = false;
+ return 0;
+}
---
+++ C++ Concepts/dataTypesModifers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    // int smallTeaPack = -100;
+    // unsigned smallTeaPack = -100; // only this will produce garbage value
+
+    unsigned smallTeaPack = 100;
+
+    long largeTeaStorage = 100000000000000;
+
+    cout<<smallTeaPack<<endl;
+    cout<<largeTeaStorage<<endl;
+
+ return 0;
+}
