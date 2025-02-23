100cfcc17f015f5f63a10f5cf182c54a8e34c2fe
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-02-23 23:37:02 +0530

    Tyoes

---
+++ C++ Concepts/dataTypes.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ //Data types are two three types
+ // primitve - already defined 
+ //derived - enhanced form of derived
+ //user defined - can defined by user too.
+//  int:
+// Usually 4 bytes (32 bits), storing values from -2,147,483,648 to 2,147,483,647.
+// float:
+// Usually 4 bytes, storing values approximately from 1.2E-38 to 3.4E+38 with about 7 decimal digits of precision.
+// double:
+// Usually 8 bytes, storing values approximately from 2.2E-308 to 1.8E+308 with about 15 decimal digits of precision.
+//char 
+//boolean
+//void
+
+//derived  - array
+//function
+//pointer
+//reference
+
+//userdefined
+//structure - almost like classes and unions
+// Enum
+// Union
+//class
+//typedef
+
+
+ return 0;
+}
--- C++ Concepts/variablesandconstants.cpp
+++ C++ Concepts/variablesandconstants.cpp
@@ -3,8 +3,26 @@ using namespace std;
 int main()
 {
     // cout<<"Welcome again"; comments
-    
-    cout<<"Welcome again";
-    cout<<"Welcome again";
+    // Only write comments where you have mentioned complex algorithms
+
+    /*
+    cout << "Welcome again";
+    cout << "Welcome again"; Multi Line comment*/
+
+    cout << "Hello World These are data types";
+    cout << endl;
+    cout << endl;
+
+    int score; // variable declared
+    score = 100; // initialise
+
+    const int uid = 232323; //won't change as Its constant now
+    // uid = 2333; wont change
+    int newScore = 1100; //both of them together
+    int hiteshBalance = 500;
+
+    // identifier = already defined special keyword which we cant use in variable
+    // only use name which makes sense.
+
     return 0;
 }
