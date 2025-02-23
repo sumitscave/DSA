8e6c1c9a3d6eb484ea1a5fe52b819cf50b750800
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-02-23 23:45:23 +0530

    Tea leaves

--- C++ Concepts/dataTypes.cpp
+++ C++ Concepts/dataTypes.cpp
@@ -1,32 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
- //Data types are two three types
- // primitve - already defined 
- //derived - enhanced form of derived
- //user defined - can defined by user too.
-//  int:
-// Usually 4 bytes (32 bits), storing values from -2,147,483,648 to 2,147,483,647.
-// float:
-// Usually 4 bytes, storing values approximately from 1.2E-38 to 3.4E+38 with about 7 decimal digits of precision.
-// double:
-// Usually 8 bytes, storing values approximately from 2.2E-308 to 1.8E+308 with about 15 decimal digits of precision.
-//char 
-//boolean
-//void
+int main()
+{
+    // Data types are two three types
+    //  primitve - already defined
+    // derived - enhanced form of derived
+    // user defined - can defined by user too.
+    //  int:
+    // Usually 4 bytes (32 bits), storing values from -2,147,483,648 to 2,147,483,647.
+    // float:
+    // Usually 4 bytes, storing values approximately from 1.2E-38 to 3.4E+38 with about 7 decimal digits of precision.
+    // double:
+    // Usually 8 bytes, storing values approximately from 2.2E-308 to 1.8E+308 with about 15 decimal digits of precision.
+    // char
+    // boolean
+    // void
 
-//derived  - array
-//function
-//pointer
-//reference
+    // derived  - array
+    // function
+    // pointer
+    // reference
 
-//userdefined
-//structure - almost like classes and unions
-// Enum
-// Union
-//class
-//typedef
+    // userdefined
+    // structure - almost like classes and unions
+    //  Enum
+    //  Union
+    // class
+    // typedef
 
+    int teaLeaves = 50;
+    float waterTemperature = 85.5;
+    double priceOfTea = 299.9999999999999999999999999;
+    char teaGrading = 'B';
+    bool isTeaReady = false; // output is 1 as
 
- return 0;
+    cout << isTeaReady << endl;
+    cout << priceOfTea << endl;
+
+    return 0;
 }
