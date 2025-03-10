51dd69e387584c822e924d53563b02c9d1fedc5e
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-03-11 00:00:00 +0530

    loopss

--- LinkedList/LL.cpp
+++ LinkedList/LL.cpp
@@ -20,11 +20,27 @@ public:
         next = nullptr;
     }
 };
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
 int main()
 {
 
-    vector<int> arr = {2, 5, 8, 7};
-    Node *y = new Node(arr[0], nullptr);
-    cout << y->data;
+    // vector<int> arr = {2, 5, 8, 7};
+    // Node *y = new Node(arr[0], nullptr);
+    // cout << y->data;
+
+    vector<int> arr = {56,4,3,2,1};
+    Node* head = convertArr2LL(arr);
+    cout<<head->data;
     return 0;
 }
--- loop/teaTask.cpp
+++ loop/teaTask.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main()
 {
     int teaCups = 5;
-    for (int i = 0; i < teaCups; i++)
+    for (int i = 1; i <= teaCups; i++)
     {
+        cout<<"making Chai.. now its "<<i<<" cup"<<endl;
     }
     return 0;
 }
