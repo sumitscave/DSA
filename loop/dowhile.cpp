1ac0980534fe889ea771790d15b7be3997557095
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-03-10 23:56:55 +0530

    loops

---
+++ loop/dowhile.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string response;
+    do
+    {
+        cout << "Do you want more tea (yes/no) - ";
+        getline(cin, response);
+    } while (response != "no" || response != "No");
+    return 0;
+}
---
+++ loop/teaTask.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int teaCups = 5;
+    for (int i = 0; i < teaCups; i++)
+    {
+    }
+    return 0;
+}
